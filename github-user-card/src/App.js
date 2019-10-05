80ab87e9d5ea392db7c8eb3a45588f7bbebeb782
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-05 13:31:06 -0400

    card is now displaying

---
+++ github-user-card/src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import axios from 'axios';
+import UserCard from './Components/UserCard.js';
+import styled from 'styled-components';
+import { Divider, Box } from '@material-ui/core';
+
+const FollowersSection = styled.section`
+  margin-top: 50px;
+`;
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      userName: 'dangus1924', 
+      user: [],
+      followers: [],
+      searchInput: '',
+      contributionsOn: false
+    } 
+
+  } 
+  
+  
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+      .then(res => {
+        console.log(res)
+        this.setState({user: res.data})
+        return res.data.followers_url; 
+      })
+      .then(followersURL => {
+        axios.get(followersURL)
+          .then(res => this.setState({followers: res.data})) 
+      })
+      .catch(err => alert(err));
+  }
+
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.userName !== this.state.userName) {
+      axios.get(`https://api.github.com/users/${this.state.userName}`)
+        .then(res => {
+          this.setState({user: res.data})
+          return res.data.followers_url;
+        })
+        .then(followersURL => {
+          axios.get(followersURL)
+            .then(res => this.setState({followers: res.data}))
+        })
+        .catch(err => alert(err));
+    }
+  };
+  
+  handleClick = (follower) => {    
+    this.setState({
+      userName: follower.login
+    })
+  }
+ 
+  handleChange = (event) => {
+    this.setState({
+      searchInput: event.target.value
+    })
+  }
+ 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      userName: (this.state.searchInput === '' ? 'dangus1924' : this.state.searchInput)
+    })
+  }
+
+  
+  toggleContributions = () => {
+    this.setState({
+      contributionsOn: !this.state.contributionsOn
+    })
+  }
+
+  render() {
+    return (
+      <Box>
+        <UserCard toggleContributions={this.toggleContributions} 
+                  contributionsOn={this.state.contributionsOn} 
+                  user={this.state.user} 
+                  />
+      </Box>
+    );
+  }
+}
+
+export default App;
